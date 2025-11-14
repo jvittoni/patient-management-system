ce1206eb492497d633f1fd8479c94f853d92cb01
Javae Vittoni <javaevittoni@gmail.com>
2025-11-14 15:50:08 -0800

    fixed spelling error

--- patient-service/src/main/java/com/jvittoni/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/jvittoni/patientservice/mapper/PatientMapper.java
@@ -7,10 +7,10 @@ public class PatientMapper {
     public static PatientResponseDTO toDTO(Patient patient) {
         PatientResponseDTO patientDTO = new PatientResponseDTO();
         patientDTO.setId(patient.getId().toString());
-        patientDTO.setName(patientDTO.getName());
-        patientDTO.setEmail(patientDTO.getEmail());
-        patientDTO.setAddress(patientDTO.getAddress());
-        patientDTO.setDateOfBirth(patientDTO.getDateOfBirth().toString());
+        patientDTO.setName(patient.getName());
+        patientDTO.setEmail(patient.getEmail());
+        patientDTO.setAddress(patient.getAddress());
+        patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
 
         return patientDTO;
     }
