2ab341883d442c725e0464ac9491cacd76eeb46e
Javae Vittoni <javaevittoni@gmail.com>
2025-11-14 15:20:21 -0800

    created patient repository

---
+++ patient-service/src/main/java/com/jvittoni/patientservice/repository/PatientRepository.java
@@ -0,0 +1,12 @@
+package com.jvittoni.patientservice.repository;
+
+import com.jvittoni.patientservice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+
+}
