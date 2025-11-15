f71ed7d4419cbfab93b2280f6a27092a725a0d4f
Javae Vittoni <javaevittoni@gmail.com>
2025-11-14 16:12:34 -0800

    created and tested get all patients controller

---
+++ patient-service/src/main/java/com/jvittoni/patientservice/controller/PatientController.java
@@ -0,0 +1,27 @@
+package com.jvittoni.patientservice.controller;
+
+import com.jvittoni.patientservice.dto.PatientRequestDTO;
+import com.jvittoni.patientservice.dto.PatientResponseDTO;
+import com.jvittoni.patientservice.service.PatientService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
+
+}
