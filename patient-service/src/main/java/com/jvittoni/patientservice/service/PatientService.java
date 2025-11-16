f6a73ffddba9cb4c80c3792c9685bce1fa341b53
Javae Vittoni <javaevittoni@gmail.com>
2025-11-16 14:10:30 -0800

    created custom exception EmailAlreadyExistsException for create patient service

--- patient-service/src/main/java/com/jvittoni/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/jvittoni/patientservice/service/PatientService.java
@@ -2,6 +2,7 @@ package com.jvittoni.patientservice.service;
 
 import com.jvittoni.patientservice.dto.PatientRequestDTO;
 import com.jvittoni.patientservice.dto.PatientResponseDTO;
+import com.jvittoni.patientservice.exception.EmailAlreadyExistsException;
 import com.jvittoni.patientservice.mapper.PatientMapper;
 import com.jvittoni.patientservice.model.Patient;
 import com.jvittoni.patientservice.repository.PatientRepository;
@@ -23,6 +24,9 @@ public class PatientService {
     }
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
+        throw new EmailAlreadyExistsException("A patient with this email " + "already exists " + patientRequestDTO.getEmail());
+        }
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
         return PatientMapper.toDTO(newPatient);
     }
