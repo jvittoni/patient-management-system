505c92f1da90728088c8e349f164c90d72eaf7e7
Javae Vittoni <javaevittoni@gmail.com>
2025-11-17 23:53:24 -0800

    created update patient service

--- patient-service/src/main/java/com/jvittoni/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/jvittoni/patientservice/service/PatientService.java
@@ -3,12 +3,15 @@ package com.jvittoni.patientservice.service;
 import com.jvittoni.patientservice.dto.PatientRequestDTO;
 import com.jvittoni.patientservice.dto.PatientResponseDTO;
 import com.jvittoni.patientservice.exception.EmailAlreadyExistsException;
+import com.jvittoni.patientservice.exception.PatientNotFoundException;
 import com.jvittoni.patientservice.mapper.PatientMapper;
 import com.jvittoni.patientservice.model.Patient;
 import com.jvittoni.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PatientService {
@@ -29,5 +32,22 @@ public class PatientService {
         }
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
         return PatientMapper.toDTO(newPatient);
+    }
+
+    public PatientResponseDTO updatePatient(UUID id, PatientRequestDTO patientRequestDTO) {
+        Patient patient = patientRepository.findById(id).orElseThrow(()-> new PatientNotFoundException("Patient not found with ID: " + id));
+
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
+            throw new EmailAlreadyExistsException("A patient with this email " + "already exists " + patientRequestDTO.getEmail());
+        }
+
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+
+        Patient updatedPatient = patientRepository.save(patient);
+
+        return PatientMapper.toDTO(updatedPatient);
     }
 }
